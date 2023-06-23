e9a7c48c709628ed82f176b4c533096ccfce9a6a
Rachel Rosalia <rachel@rosalia.me>
2023-06-23 15:26:42 -0700

    Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    letterSpacing: {
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      // wider: '.05em',
+      wider: '.1em',
+      // widest: '.1em',
+      widest: '.25em',
+    },
     extend: {},
   },
   plugins: [],
